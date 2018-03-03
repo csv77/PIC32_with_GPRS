d661fa4e5d9d287bd5d6e10c5aae2211aa5fc47b
Vojnovits Csaba <Vojnovits Csaba@CSABI>
2018-03-03 20:23:09 +0100

    commit GSM2Click

--- app.c
+++ app.c
@@ -23,7 +23,7 @@ unsigned char rsp11[100] = "";
 unsigned char rsp12[100] = "";
 unsigned char rsp13[100] = "";
 unsigned char rsp14[100] = "";
-GSM_STATES gsm_state;
+GSM_STATES gsm_state = GSM_POWER_OFF;
 
 void main (void) {
     while(1) {
@@ -38,79 +38,186 @@ void main (void) {
                 HTS221Init();
                 HTS221Calibration();
                 UARTInit();
-                GSMStart();
                 
-                Wait(10000);
-                LED1On();
-
-                SendATCommand("AT");
-                GetResponse(rsp1, 200);
-
-                SendATCommand("AT+CREG?");
-                GetResponse(rsp2, 200);
-
-                SendATCommand("AT+QIMODE=0");
-                GetResponse(rsp3, 200);
-
-                SendATCommand("AT+QICSGP=1,\"internet.telekom\"");
-                GetResponse(rsp4, 400);
-
-                SendATCommand("AT+QIREGAPP");
-                GetResponse(rsp5, 400);
-
-                SendATCommand("AT+QIACT");
-                GetResponse(rsp6, 400);
-
-                SendATCommand("AT+QIDNSIP=1");
-                GetResponse(rsp7, 400);
-
-                SendATCommand("AT+QIOPEN=\"TCP\",\"vocsabi.asuscomm.com\",\"2048\"");
-                GetResponse(rsp8, 800);
-
-                SendATCommand("AT+QISTAT");
-                GetResponse(rsp14, 200);
-
                 while(1) {
-                    if(SW2 == 0) {
-                        Wait(1);
-                        if(SW2 == 1) {
-                            TRHCalc();
-                            
-                            SendATCommand("AT+QISEND=4");
-                            GetResponse(rsp9, 200);
-
-                            SendDataFloat(T);
-                            GetResponse(rsp10, 200);
-                            Wait(2000);
-
-                            SendATCommand("AT+QISEND=4");
-                            GetResponse(rsp9, 200);
-
-                            SendDataFloat(RH);
-                            GetResponse(rsp10, 200);
-                            Wait(2000);
-                        }
-                    }
-                    if(SW1 == 0) {
-                        Wait(1);
-                        if(SW1 == 1) {
+                    switch(gsm_state) {
+                        case GSM_POWER_OFF :
+                            GSMStart();
+                            LED1On();
                             break;
-                        }
+                        case GSM_POWER_ON :
+                            SendATCommand("AT");
+                            GetResponse(rsp1, 200);
+
+                            SendATCommand("AT+CREG?");
+                            GetResponse(rsp2, 200);
+                            
+                            if(strstr(rsp2, "CREG: 0,1") != NULL) {
+                                gsm_state = GSM_CREG_OK;
+                            }
+                            break;
+                        case GSM_CREG_OK :
+                            SendATCommand("AT+QIMODE=0");
+                            GetResponse(rsp3, 200);
+
+                            SendATCommand("AT+QICSGP=1,\"internet.telekom\"");
+                            GetResponse(rsp4, 400);
+                            
+                            if(strstr(rsp4, "OK")) {
+                                gsm_state = GSM_QIREGAPP_OK;
+                            }
+                            break;
+                        case GSM_QIREGAPP_OK :
+                            SendATCommand("AT+QIREGAPP");
+                            GetResponse(rsp5, 400);
+
+                            SendATCommand("AT+QIACT");
+                            GetResponse(rsp6, 400);
+
+                            SendATCommand("AT+QIDNSIP=1");
+                            GetResponse(rsp7, 400);
+
+                            SendATCommand("AT+QIOPEN=\"TCP\",\"vocsabi.asuscomm.com\",\"2048\"");
+                            GetResponse(rsp8, 800);
+
+                            if(strstr(rsp8, "CONNECT OK") != NULL) {
+                                gsm_state = GSM_TCP_CONNECTION_OPENED;
+                            }
+                            break;
+                        case GSM_TCP_CONNECTION_OPENED :
+                            while(1) {
+                                if(SW2 == 0) {
+                                    Wait(1);
+                                    if(SW2 == 1) {
+                                        TRHCalc();
+
+                                        SendATCommand("AT+QISEND=4");
+                                        GetResponse(rsp9, 200);
+
+                                        SendDataFloat(T);
+                                        GetResponse(rsp10, 200);
+                                        Wait(1000);
+
+                                        SendATCommand("AT+QISEND=4");
+                                        GetResponse(rsp9, 200);
+
+                                        SendDataFloat(RH);
+                                        GetResponse(rsp10, 200);
+                                        Wait(1000);
+                                    }
+                                }
+                                if(SW1 == 0) {
+                                    Wait(1);
+                                    if(SW1 == 1) {
+                                        break;
+                                    }
+                                }
+                            }
+                            gsm_state = GSM_TCP_DATA_SENT;
+                            break;
+                        case GSM_TCP_DATA_SENT :
+                            SendATCommand("AT+QICLOSE");
+                            GetResponse(rsp12, 200);
+
+                            SendATCommand("AT+QIDEACT");
+                            GetResponse(rsp13, 200);
+                            gsm_state = GSM_TCP_CONNECTION_CLOSED;
+                            break;
+                    }
+                    if(gsm_state == GSM_TCP_CONNECTION_CLOSED) {
+                        gsm_state == GSM_POWER_ON;
+                        break;
                     }
                 }
-                
-                SendATCommand("AT+QISACK");
-                GetResponse(rsp11, 200),
-
-                SendATCommand("AT+QICLOSE");
-                GetResponse(rsp12, 200);
-
-                SendATCommand("AT+QIDEACT");
-                GetResponse(rsp13, 200);
-                
                 LED1Off();
             }
         }
     }
 }
 
+//while(1) {
+//        if(SW1 == 0) {
+//            Wait(1);
+//            if(SW1 == 1) {
+//                mJTAGPortEnable(0);
+//                LedInit();
+//                ButtonInit();
+//
+//                I2CInit();
+//                HTS221Init();
+//                HTS221Calibration();
+//                UARTInit();
+//                GSMStart();
+//                
+//                Wait(10000);
+//                LED1On();
+//
+//                SendATCommand("AT");
+//                GetResponse(rsp1, 200);
+//
+//                SendATCommand("AT+CREG?");
+//                GetResponse(rsp2, 200);
+//
+//                SendATCommand("AT+QIMODE=0");
+//                GetResponse(rsp3, 200);
+//
+//                SendATCommand("AT+QICSGP=1,\"internet.telekom\"");
+//                GetResponse(rsp4, 400);
+//
+//                SendATCommand("AT+QIREGAPP");
+//                GetResponse(rsp5, 400);
+//
+//                SendATCommand("AT+QIACT");
+//                GetResponse(rsp6, 400);
+//
+//                SendATCommand("AT+QIDNSIP=1");
+//                GetResponse(rsp7, 400);
+//
+//                SendATCommand("AT+QIOPEN=\"TCP\",\"vocsabi.asuscomm.com\",\"2048\"");
+//                GetResponse(rsp8, 800);
+//
+//                SendATCommand("AT+QISTAT");
+//                GetResponse(rsp14, 200);
+//
+//                while(1) {
+//                    if(SW2 == 0) {
+//                        Wait(1);
+//                        if(SW2 == 1) {
+//                            TRHCalc();
+//                            
+//                            SendATCommand("AT+QISEND=4");
+//                            GetResponse(rsp9, 200);
+//
+//                            SendDataFloat(T);
+//                            GetResponse(rsp10, 200);
+//                            Wait(2000);
+//
+//                            SendATCommand("AT+QISEND=4");
+//                            GetResponse(rsp9, 200);
+//
+//                            SendDataFloat(RH);
+//                            GetResponse(rsp10, 200);
+//                            Wait(2000);
+//                        }
+//                    }
+//                    if(SW1 == 0) {
+//                        Wait(1);
+//                        if(SW1 == 1) {
+//                            break;
+//                        }
+//                    }
+//                }
+//                
+//                SendATCommand("AT+QISACK");
+//                GetResponse(rsp11, 200),
+//
+//                SendATCommand("AT+QICLOSE");
+//                GetResponse(rsp12, 200);
+//
+//                SendATCommand("AT+QIDEACT");
+//                GetResponse(rsp13, 200);
+//                
+//                LED1Off();
+//            }
+//        }
+//    }
